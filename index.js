3e4bd97b48bf6a5abff4eb531fefc1d738ad89a0
radville <laura.radville@gmail.com>
2019-10-28 10:10:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+function saturdayFun(target="roller-skate") {
+    return `This Saturday, I want to ${target}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(type="special") {
+        return `You are ${flair}${type}${flair}!`
+    }
+}
+
+// const Calculator = new Object()
+let Calculator = {
+    add : function(num1, num2) {
+        return num1+num2
+    },
+    subtract : function(num1, num2) {
+        return num1-num2
+    },
+    multiply : function(num1, num2) {
+        return num1*num2
+    },
+    divide : function(num1, num2) {
+        return num1/num2
+    }
+}
+
+function actionApplyer(integer, functions) {
+    if (functions.length === 0){
+        return integer
+    } else {
+        let total = functions[0](integer);
+        functions.slice(1).forEach (function(e) {
+            total = e(total)
+            console.log(total)
+        });
+        return total}
+}
+
+
+
